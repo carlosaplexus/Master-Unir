4f5d9fd123a21039e716234bd49ad9ad23906463
Carlos Alonso <carlosa.plexus@hotmail.com>
2026-01-23 11:24:29 +0100

    Cambio puerto a 3000

--- backend/app.js
+++ backend/app.js
@@ -21,5 +21,5 @@ const itemsRouter = require("./routes/items");
 app.use("/api/items", itemsRouter);
 
 // Puerto
-const PORT = process.env.PORT || 3008;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor Node escuchando en puerto ${PORT}`));
